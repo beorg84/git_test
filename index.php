04b3e052b2e03e2d48b16643db04c808d17f5a2a
Mirko Rapicano <M.Rapicano@logotel.it>
2015-10-15 11:12:14 +0200

    commit C2

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 echo "COMMIT C1";
+echo "COMMIT C2";
 // Set flag that this is a parent file.
 define('_JEXEC', 1);
 define('DS', DIRECTORY_SEPARATOR);
